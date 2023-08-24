cbc10d66aaa483b60e8a6a1f9217ec97f66c5e42
Faissal-00 <faysalfayfa@gmail.com>
2023-08-24 21:31:31 +0100

    PowerBI

---
+++ SQLQuery.sql
@@ -0,0 +1,9 @@
+use supermarketdb
+select *from dimBranch
+select *from dimCity
+select *from dimCustomerType
+select *from dimDate
+select *from dimGender
+select *from dimPayment
+select *from dimProductLine
+select *from factSales
